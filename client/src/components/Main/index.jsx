d66a1b8d4b9f73a807b88338858e7dadf3e9cd6e
Johan Hermansson <karljohan.hermanssons@gmail.com>
2022-12-30 09:38:59 +0100

    Adds chat to logged in users

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,12 +1,19 @@
+import Chat from '../Chat';
+import { io } from 'socket.io-client';
+
 const Main = () => {
   const handleLogout = () => {
     localStorage.removeItem('token');
     window.location.reload();
   };
 
+  const server = 'http://localhost:5005/';
+  const socket = io.connect(server);
+
   return (
     <div>
       <h1>Inloggad</h1>
+      <Chat socket={socket} userName={'chatUser'} room={'chatRoom'} />
       <button onClick={handleLogout}>Log Out</button>
     </div>
   );
